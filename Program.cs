2f2b52c206b4db979937b951a5a555bc9eb57cc0
Bruno Garcia <bruno@brunogarcia.com>
2018-04-20 11:17:05 +0200

    repro: [ERROR] FATAL UNHANDLED EXCEPTION: System.TypeLoadException: Could not resolve type with token 0100000d (from typeref, class/assembly System.Runtime.InteropServices.RuntimeInformation, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace runtime_info_mono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Process Architecture: " + System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture);
+        }
+    }
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+dotnet build -c Release
+mono bin/Release/net471/runtime-info-mono.exe
